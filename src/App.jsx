29e1dfcf4d42ea6fc7006e4513c25e3031509167
Asadbek20032003 <asadbeksolijonov35@gmail.com>
2024-04-03 10:54:13 +0300

    Todo project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ import React from 'react'
+import './App.css'
+import ToDoList from './component/ToDoList'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='todo-app'>
+      <ToDoList/>
+    </div>
+    </>
+  )
+}
+
+export default App
